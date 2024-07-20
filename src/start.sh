151c9375e125897ea20909a0d9187bcf82c57d27
sky-admin <546485299@qq.com>
2024-07-21 01:34:01 +0800

    feat: add deps for adetailer

--- src/start.sh
+++ src/start.sh
@@ -3,7 +3,7 @@
 echo "Worker Initiated"
 
 echo "Starting WebUI API"
-python /workspace/sd/stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --lowram --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
+python /workspace/sd/stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --lowram --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check --no-hashing --no-download-sd-model &
 
 echo "Starting RunPod Handler"
 python -u /handler.py
